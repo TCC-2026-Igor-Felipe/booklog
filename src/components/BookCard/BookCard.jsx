2f64b6b9b70009c82434775f8b2211824e919fb9
felxx <igorfelipemonteirocontato@gmail.com>
2026-05-21 10:16:17 -0300

    feat: implementa o componente BookCard para exibir título e autor do livro

--- src/components/BookCard/BookCard.jsx
+++ src/components/BookCard/BookCard.jsx
@@ -1,3 +1,10 @@
-export default function BookCard() {
-  return <div />;
+export default function BookCard({ livro }) {
+  if (!livro) return null;
+
+  return (
+    <div>
+      <h2>{livro.titulo}</h2>
+      <p>{livro.autor}</p>
+    </div>
+  );
 }
