9944af95d2e37f378ab88935ccfb799133065149
felxx <igorfelipemonteirocontato@gmail.com>
2026-05-21 10:38:48 -0300

    feat: adiciona renderização de resultados na SearchView

--- src/views/SearchView/SearchView.jsx
+++ src/views/SearchView/SearchView.jsx
@@ -1,9 +1,16 @@
 import SearchBar from '../../components/SearchBar/SearchBar';
+import BookCard from '../../components/BookCard/BookCard';
 
-export default function SearchView() {
+export default function SearchView({ resultados = [] }) {
   return (
     <main>
       <SearchBar />
+      
+      <div className="resultados-lista">
+        {resultados.map((livro, index) => (
+          <BookCard key={index} livro={livro} />
+        ))}
+      </div>
     </main>
   );
 }
