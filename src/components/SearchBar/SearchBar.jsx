5861578b2f5ea42d24f751ac6ddad9ed52c66ea4
felxx <igorfelipemonteirocontato@gmail.com>
2026-05-21 09:49:30 -0300

    feat: implementa componente de barra de busca

---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,10 @@
+export default function SearchBar() {
+  return (
+    <div>
+      <input 
+        type="text" 
+        placeholder="Buscar por título, autor ou ISBN..." 
+      />
+    </div>
+  );
+}
