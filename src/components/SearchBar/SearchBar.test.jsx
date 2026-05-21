73d156bc0ba425d204e5ace576e2fd0606c81abb
felxx <igorfelipemonteirocontato@gmail.com>
2026-05-21 09:57:30 -0300

    test: deve chamar a função onSearch passando o texto digitado no componente SearchBar

--- src/components/SearchBar/SearchBar.test.jsx
+++ src/components/SearchBar/SearchBar.test.jsx
@@ -9,4 +9,16 @@ describe('SearchBar Component', () => {
 
         expect(inputElement).toBeInTheDocument();
     });
+
+    it('deve chamar a função onSearch passando o texto digitado', () => {
+        const mockOnSearch = vi.fn();
+        
+        render(<SearchBar onSearch={mockOnSearch} />);
+        
+        const inputElement = screen.getByPlaceholderText('Buscar por título, autor ou ISBN...');
+            
+        fireEvent.change(inputElement, { target: { value: 'Tolkien' } });
+        
+        expect(mockOnSearch).toHaveBeenCalledWith('Tolkien');
+    });
 });
