f78952dc007d43df2362e86414980898ed8ac928
felxx <igorfelipemonteirocontato@gmail.com>
2026-05-21 10:02:02 -0300

    feat: adiciona a função onSearch no componente SearchBar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,9 +1,10 @@
-export default function SearchBar() {
+export default function SearchBar({ onSearch }) {
   return (
     <div>
       <input 
         type="text" 
         placeholder="Buscar por título, autor ou ISBN..." 
+        onChange={(evento) => onSearch(evento.target.value)}
       />
     </div>
   );
