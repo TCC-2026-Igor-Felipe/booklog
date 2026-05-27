1e0cda5bb7e1a268a23f3124cbb2b680f7c70eae
felxx <igorfelipemonteirocontato@gmail.com>
2026-05-27 11:26:04 -0300

    feat: adiciona catálogo de livros ao projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,11 @@
 import SearchView from './views/SearchView/SearchView';
+import catalogo from './data/catalogo.json';
 
 export default function App() {
-  const livrosExemplo = [
-    { 
-      titulo: 'O Senhor dos Anéis', 
-      autor: 'J.R.R. Tolkien', 
-      genero: 'Fantasia',
-      ano: 1954,
-      capaUrl: 'https://placehold.co/625x1000' 
-    },
-    { 
-      titulo: '1984', 
-      autor: 'George Orwell', 
-      genero: 'Ficção',
-      ano: 1949,
-      capaUrl: 'https://placehold.co/625x1000' 
-    },
-    { 
-      titulo: 'Duna', 
-      autor: 'Frank Herbert', 
-      genero: 'Ficção',
-      ano: 1965,
-      capaUrl: 'https://placehold.co/625x1000' 
-    },
-    { 
-      titulo: 'Clean Code', 
-      autor: 'Robert C. Martin', 
-      genero: 'Tecnologia',
-      ano: 2008,
-      capaUrl: 'https://placehold.co/625x1000' 
-    }
-  ];
-
   return (
     <div>
-      <h1>Meu Booklog</h1>
-      <SearchView resultados={livrosExemplo} />
+      <h1>Booklog</h1>
+      <SearchView resultados={catalogo} />
     </div>
   );
 }
