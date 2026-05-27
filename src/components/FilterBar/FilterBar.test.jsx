b1471f3915fd9815d00f9f26e82c6f0b5d08211a
felxx <igorfelipemonteirocontato@gmail.com>
2026-05-27 10:48:11 -0300

    test: deve chamar a função onFilterChange quando o usuário seleciona um género

--- src/components/FilterBar/FilterBar.test.jsx
+++ src/components/FilterBar/FilterBar.test.jsx
@@ -1,15 +1,27 @@
-import { render, screen } from '@testing-library/react';
-import { describe, it, expect } from 'vitest';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { describe, it, expect, vi } from 'vitest';
 import FilterBar from './FilterBar';
 
 describe('FilterBar Component', () => {
   it('deve renderizar os seletores de gênero e ano de publicação', () => {
     render(<FilterBar />);
-    
+
     const seletorGenero = screen.getByRole('combobox', { name: /gênero/i });
     const seletorAno = screen.getByRole('combobox', { name: /ano/i });
-    
+
     expect(seletorGenero).toBeInTheDocument();
     expect(seletorAno).toBeInTheDocument();
+  });
+
+  it('deve chamar a função onFilterChange quando o usuário seleciona um género', () => {
+    const mockOnFilterChange = vi.fn();
+
+    render(<FilterBar onFilterChange={mockOnFilterChange} />);
+
+    const seletorGenero = screen.getByRole('combobox', { name: /gênero/i });
+
+    fireEvent.change(seletorGenero, { target: { value: 'Ficção' } });
+
+    expect(mockOnFilterChange).toHaveBeenCalled();
   });
 });
