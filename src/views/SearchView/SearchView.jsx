68f0c8b956d2f31930f4dbeb44b9fcd516f663d8
felxx <igorfelipemonteirocontato@gmail.com>
2026-05-27 10:44:20 -0300

    feat: implementa a função de pesquisa na SearchView

--- src/views/SearchView/SearchView.jsx
+++ src/views/SearchView/SearchView.jsx
@@ -1,15 +1,25 @@
+import { useState } from 'react'; // 1. Importamos o hook de estado do React
 import SearchBar from '../../components/SearchBar/SearchBar';
 import FilterBar from '../../components/FilterBar/FilterBar';
 import BookCard from '../../components/BookCard/BookCard';
 
 export default function SearchView({ resultados = [] }) {
+  const [termoBusca, setTermoBusca] = useState('');
+  const livrosFiltrados = resultados.filter((livro) => {
+    const termo = termoBusca.toLowerCase();
+    const titulo = livro.titulo.toLowerCase();
+    const autor = livro.autor.toLowerCase();
+
+    return titulo.includes(termo) || autor.includes(termo);
+  });
+
   return (
     <main>
-      <SearchBar />
+      <SearchBar onSearch={setTermoBusca} />
       <FilterBar />
-      
+
       <div className="resultados-lista">
-        {resultados.map((livro, index) => (
+        {livrosFiltrados.map((livro, index) => (
           <BookCard key={index} livro={livro} />
         ))}
       </div>
